fa79319f0e6d529b5c23ff3dc5d859feca9cc5b9
WendelSousaSilva <60802640+WendelSousaSilva@users.noreply.github.com>
2020-06-04 02:20:18 -0300

    JWT- Retornando TOKEN

--- src/br/com/pentagono/estoque/conf/SecurityConfig.java
+++ src/br/com/pentagono/estoque/conf/SecurityConfig.java
@@ -1,43 +1,90 @@
 package br.com.pentagono.estoque.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import br.com.pentagono.estoque.filters.JwtRequestFilter;
+
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService usuarios;
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(usuarios).passwordEncoder(new BCryptPasswordEncoder());
+		auth.userDetailsService(usuarios).passwordEncoder(passwordEncoder());
+	}
+	
+	@Order(1)
+	@Configuration
+	public static class ApiConfig extends WebSecurityConfigurerAdapter {
+		
+		@Autowired
+		private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+		
+		@Autowired
+		private JwtRequestFilter jwtRequestFilter;
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			
+			http.antMatcher("/api/**").authorizeRequests()
+				.antMatchers("/api/fornecedores/**").permitAll()
+				.antMatchers("/api/usuarios/login").permitAll()
+				.anyRequest().authenticated()
+				.and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().csrf().disable();
+			
+			http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+		}
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Order(2)
+	@Configuration
+	public static class WebConfig extends WebSecurityConfigurerAdapter {
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().
-			antMatchers("/").permitAll().
-			antMatchers("/api/**").permitAll().
-			antMatchers("/home").permitAll().
-			antMatchers("/front/**").permitAll().
-			antMatchers("/static/**").permitAll().
-			antMatchers("/perfis").hasRole("ADMIN").
-			antMatchers("/usuarios/**").hasRole("ADMIN").
-			anyRequest().authenticated().
-			and().
-			formLogin().defaultSuccessUrl("/").loginPage("/login").permitAll().
-			and().
-			logout().logoutSuccessUrl("/").logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-			.and().csrf().disable();
+			http.authorizeRequests().
+				antMatchers("/").permitAll().
+				antMatchers("/home").permitAll().
+				antMatchers("/static/**").permitAll().
+				antMatchers("/perfis").hasRole("ADMIN").
+				antMatchers("/usuarios/**").hasRole("ADMIN").
+				anyRequest().authenticated().
+				and().
+				formLogin().defaultSuccessUrl("/").loginPage("/login").permitAll().
+				and().
+				logout().logoutSuccessUrl("/").logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		}		
 	}
-
-
 }
--- src/br/com/pentagono/estoque/controllers/UsuarioApiController.java
+++ src/br/com/pentagono/estoque/controllers/UsuarioApiController.java
@@ -1,61 +1,53 @@
 package br.com.pentagono.estoque.controllers;
 
-import java.security.Principal;
-import java.util.Base64;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.pentagono.estoque.DTO.JwtRequestDTO;
+import br.com.pentagono.estoque.DTO.JwtResponseDTO;
 import br.com.pentagono.estoque.daos.UsuarioDAO;
-import br.com.pentagono.estoque.models.Usuario;
+import br.com.pentagono.estoque.utils.JwtTokenUtils;
 
-@Transactional
+@CrossOrigin
 @RestController
 @RequestMapping("/api/usuarios")
-@CrossOrigin
 public class UsuarioApiController {
-	
+
+	@Autowired
+	private AuthenticationManager managerAuthSpring;
+
+	@Autowired
+	private JwtTokenUtils jwtTokenUtils;
+
 	@Autowired
 	private UsuarioDAO usuarioDAO;
-	
-	
-	@GetMapping
-	public List<Usuario> listar() {
-		return usuarioDAO.listarTodos();
+
+	@PostMapping(value = "/login")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequestDTO authenticationRequest) throws Exception {
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+		final UserDetails userDetails = usuarioDAO.loadUserByUsername(authenticationRequest.getUsername());
+		final String token = jwtTokenUtils.generateToken(userDetails);
+		return ResponseEntity.ok(new JwtResponseDTO(token));
 	}
 
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Usuario> listarPorId(@PathVariable String id) {
-		
-		Usuario usuario = usuarioDAO.buscarPorId(id);
-
-		if (usuario == null) {
-			return new ResponseEntity<Usuario>(HttpStatus.NOT_FOUND);
-		}		
-		
-		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
-	}
-	
-	@PostMapping
-	public Usuario salvarUsuario(@RequestBody Usuario usuarioApiQueSeraSalvo) {
-		return usuarioDAO.salvarApi(usuarioApiQueSeraSalvo);
-	}
-	
-
-
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			managerAuthSpring.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
 	}
 
+}
